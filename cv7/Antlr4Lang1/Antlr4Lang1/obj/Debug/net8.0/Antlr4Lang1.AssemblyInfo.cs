eaefd85d3dd9a39170a5f3348f3bad927a3d8de7
Jan-Kryštof Zahradník <jendazah@gmail.com>
2024-04-01 22:05:15 +0200

    wp

--- cv7/Antlr4Lang1/Antlr4Lang1/obj/Debug/net8.0/Antlr4Lang1.AssemblyInfo.cs
+++ cv7/Antlr4Lang1/Antlr4Lang1/obj/Debug/net8.0/Antlr4Lang1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Antlr4Lang1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f7b5be1a473dd6376f6beb79ab03f4af81ca3c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1475254babfa08e3383166bb7ffab78aaff19153")]
 [assembly: System.Reflection.AssemblyProductAttribute("Antlr4Lang1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Antlr4Lang1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
